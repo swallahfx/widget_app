62555d64a5365e256482375717f9c997780e37a2
saheedEarly <saheed@getearlybird.ai>
2025-05-21 23:59:34 +0100

    fixes

--- widget_api/settings.py
+++ widget_api/settings.py
@@ -88,7 +88,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 STATIC_URL = "static/"
 
-# Default primary key field type
+# Default primary key type
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 # REST Framework settings
